cbe7958f7bb03ab39f7ca07ec24bf34a6ab41783
pavankrishnan77 <pavankrishnan1993@gmail.com>
2019-07-20 14:19:33 +0530

    Modified

--- src/Patterns/Pattern1_4.java
+++ src/Patterns/Pattern1_4.java
@@ -2,14 +2,13 @@ package Patterns;
 
 public class Pattern1_4 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		int counter = 1;
-		for(int i=1; i<=10; i++)
+		for(int i=1; i<=5; i++)
 		{
-			for(int j=1; j<=10; j++)
+			for(int j=1; j<=i; j++)
 			{
-				System.out.print(counter++);
+				System.out.print(i);
 			}
 			System.out.println();
 		}
@@ -17,13 +16,8 @@ public class Pattern1_4
 }
 
 //Output:
-//12345678910
-//11121314151617181920
-//21222324252627282930
-//31323334353637383940
-//41424344454647484950
-//51525354555657585960
-//61626364656667686970
-//71727374757677787980
-//81828384858687888990
-//919293949596979899100
+//1
+//22
+//333
+//4444
+//55555
